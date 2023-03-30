8348f89799547b4aa8e4d630ad93f7434cef89cf
piotrszewczuk <60941051+piotrszewczuk@users.noreply.github.com>
2023-03-30 10:01:19 +0200

    Script files for Vagrantfile

---
+++ scripts/rancher.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Common setup for Rancher
+
+set -euxo pipefail
+
+# Variable Declaration
+
+# DNS Setting
+if [ ! -d /etc/systemd/resolved.conf.d ]; then
+	sudo mkdir /etc/systemd/resolved.conf.d/
+fi
+cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
+[Resolve]
+DNS=${DNS_SERVERS}
+EOF
+
+sudo systemctl restart systemd-resolved
+
+# disable swap
+sudo swapoff -a
+
+# keeps the swap off during reboot
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+sudo apt-get update -y
+
+# Install Docker
+sudo apt-get update
+sudo apt install -y docker.io
+sudo systemctl enable --now docker
+
